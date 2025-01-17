c3bea1bcaeb14a517cefd5c3063237f1c2758cd6
Arthur bernardo <arthurutschgg@gmail.com>
2025-01-17 13:42:01 -0300

    feat: add builder services add db db context

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -4,27 +4,26 @@ using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ScreenSoundContext>();
+builder.Services.AddTransient<DAL<Artista>>();
 
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 var app = builder.Build();
 
-app.MapGet("/Artistas", () =>
+app.MapGet("/Artistas", ([FromServices] DAL<Artista> dal) =>
 {
-    var dal = new DAL<Artista>(new ScreenSoundContext());
     return Results.Ok(dal.Listar());
 });
 
-app.MapGet("/Artistas/{nome}", (string nome) =>
+app.MapGet("/Artistas/{nome}", ([FromServices] DAL<Artista> dal, string nome) =>
 {
-    var dal = new DAL<Artista>(new ScreenSoundContext());
     var artista =  dal.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
 
     return artista is null ? Results.NotFound() : Results.Ok(artista);
 });
 
-app.MapPost("/Artistas", ([FromBody] Artista artista) =>
+app.MapPost("/Artistas", ([FromServices] DAL<Artista> dal, [FromBody] Artista artista) =>
 {
-    var dal = new DAL<Artista>(new ScreenSoundContext());
     dal.Adicionar(artista);
     return Results.Ok();
 
